bc7b4a124a4b010027ed14f387466625cb58c2ec
IT22354310 <127657544+IT22354310@users.noreply.github.com>
2023-03-28 04:36:27 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Tute03/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	 float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
+	 char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+	 cout << "No\t\t\t" << "Name\t\t " << "Marks" << endl;
+	 
+	 for (int r = 0; r < 5; r++) {
+	 cout << setw(5) << r+1<< setw(15) << names[r]<< setw(10) << setiosflags(ios::fixed)<< setprecision(2) << marks[r] << endl;
+	 }
+	 return 0;
+
+}
